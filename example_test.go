de6ba1819e5522415d88393a7cfab28ec15adbe8
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-05-16 12:50:12 +0900

    wip insert restaurants

--- example_test.go
+++ example_test.go
@@ -9,6 +9,8 @@ import (
 	"log"
 
 	"github.com/antonve/go-sqlboiler-experiment/models"
+	"github.com/paulmach/orb"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
@@ -25,11 +27,31 @@ func ExampleQueries() {
 	runMigrations(psql)
 
 	ctx := context.Background()
+
+	// create restaurants
+	cocoId := createRestaurant(psql, "CoCo Ichibanya Ebisu", 35.64699825984844, 139.71194575396922)
+	shakeId := createRestaurant(psql, "SHAKE SHACK Ebisu", 35.64669248211187, 139.70949784477963)
+	ichiranId := createRestaurant(psql, "Ichiran Ramen Shinjuku", 35.69079988476277, 139.70286473414785)
+	torikiId := createRestaurant(psql, "Torikizoku Shinjuku", 35.68918337273537, 139.70249991934935)
+
 	restaurants, err := models.Restaurants().All(ctx, psql)
-	fmt.Print(restaurants)
+	fmt.Print(restaurants, cocoId, shakeId, ichiranId, torikiId)
 
 	// output:
 	// []
+}
+
+func createRestaurant(psql *sql.DB, name string, long, lat float64) int64 {
+	var r models.Restaurant
+	r.Name = name
+	r.Location = orb.Point{long, lat}
+
+	err := r.Insert(context.Background(), psql, boil.Infer())
+	if err != nil {
+		log.Fatalf("cannot create restaurant: %v", err)
+	}
+
+	return r.ID
 }
 
 func runMigrations(psql *sql.DB) {
